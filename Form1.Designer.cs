7e642d690b64a4b02311673c67839570e0717833
k_maezawa <k_maezawa@KUNIYOSHI_MAIN>
2022-10-08 11:54:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace TestSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtTaxPrice = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "税抜価格";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "税込価格";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(100, 37);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 23);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // txtTaxPrice
+            // 
+            this.txtTaxPrice.Location = new System.Drawing.Point(100, 82);
+            this.txtTaxPrice.Name = "txtTaxPrice";
+            this.txtTaxPrice.ReadOnly = true;
+            this.txtTaxPrice.Size = new System.Drawing.Size(100, 23);
+            this.txtTaxPrice.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(125, 133);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "計算する";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 198);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtTaxPrice);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtTaxPrice;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TestSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Debug.WriteLine("Hello World!");
+            //MessageBox.Show("Hello World!");
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // 文字から数値へ変換する
+            int price;
+            bool success = int.TryParse(this.txtPrice.Text, out price);
+
+            if(success)
+            {
+                // 消費税を計算する
+                int taxPrice = (int)(price * 1.1);
+                this.txtTaxPrice.Text = taxPrice.ToString();
+            }
+            else
+            {
+                MessageBox.Show("数値に変換できませんでした");
+            }
+        }
+    }
+}
